41c00bc806c0ff885b89d8ca79e455be79d1fb67
Maxime RONDEAU <113727341+maxpline83@users.noreply.github.com>
2023-04-04 12:30:00 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -27,24 +27,17 @@ def chatgpt():
     )
     return completion['choices'][0]['text']
 
-
-@app.route('/generate-code')
+@app.route('/generate_code')
 def generate_code():
     args = request.args
-    language = args.get("language")
-    content = args.get("content")
-    prompt = f"Generate code in {language} programming language based on the following content:\n\n{content}\n\nCode:"
-    completion = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
+    coding_language = args.get("coding_language")
+    code_request = args.get("code_request")
+
+    message = f"Write {code_request} in {coding_language}."
+    print(message)
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
     )
-    return completion.choices[0].text.strip()
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+    return completion['choices'][0]['message']['content']
