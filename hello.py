339d8a7354780e6f96be48a775a9c12ea9d70d31
Maxime RONDEAU <113727341+maxpline83@users.noreply.github.com>
2023-04-04 12:38:51 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask,request
 import os
 import openai
 
@@ -11,21 +11,17 @@ openai.api_key = os.environ.get('OPENAI_KEY')
 def index():
     return "<h1>Hello, World!</h1>"
 
-
 @app.route('/chatgpt')
 def chatgpt():
     args = request.args
-    message = args.get("message")
+    message =args.get("message")
     print(message)
     completion = openai.ChatCompletion.create(
-        model="text-davinci-002",
-        prompt=message,
-        temperature=0.7,
-        max_tokens=1024,
-        n=1,
-        stop=None
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}]
     )
-    return completion['choices'][0]['text']
+    return completion['choices'][0]['message']['content']
+
 
 @app.route('/generate_code')
 def generate_code():
